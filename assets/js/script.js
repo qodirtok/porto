8b91a981eb8d33ecb9d3738880d3800201e6995e
qodirtok <qodirtok@gmail.com>
2023-05-18 09:50:55 +0700

    part 9

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-$('.nav-ico').click(function(event) {
+$(".nav-ico").click(function (event) {
   // console.log('masuk');
-  $('.main-nav').toggleClass('active');
-  $(this).toggleClass('active');
+  $(".main-nav").toggleClass("active");
+  $(this).toggleClass("active");
+});
+
+window.addEventListener("scroll", function (params) {
+  var header = this.document.querySelector("header");
+  header.classList.toggle("sticky", window.screenY > 0);
 });
