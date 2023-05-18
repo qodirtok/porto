f3383c84a1ccdb4adeb729dd5fb44ab573a0e79e
qodirtok <qodirtok@gmail.com>
2023-05-18 11:30:28 +0700

    part 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,3 @@ $(".nav-ico").click(function (event) {
   $(".main-nav").toggleClass("active");
   $(this).toggleClass("active");
 });
-
-window.addEventListener("scroll", function (params) {
-  var header = this.document.querySelector("header");
-  header.classList.toggle("sticky", window.screenY > 0);
-});
